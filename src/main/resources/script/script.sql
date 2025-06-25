9db6e4565f4215c575428af85e812988c15db85f
Davide Tersgni <31887787+Tersigdav@users.noreply.github.com>
2025-06-25 15:32:19 +0200

    script sql cambiato

--- src/main/resources/script/script.sql
+++ src/main/resources/script/script.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS campaign (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL,
-    is_private BOOLEAN,
+    be_private BOOLEAN,
     password VARCHAR(50),
     description VARCHAR(500) NOT NULL,
     rule_version VARCHAR(20) NOT NULL,
